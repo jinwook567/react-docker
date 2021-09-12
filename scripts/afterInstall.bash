2c59843c814f3882d6a1f38b6cd71b119c42d630
kinoooooook123 <31956127+kinoooooook123@users.noreply.github.com>
2021-09-12 17:12:04 +0900

    after install fix

--- scripts/afterInstall.bash
+++ scripts/afterInstall.bash
@@ -1,5 +1,4 @@
 docker login -u we2dinc -p 7a129db5-f2d4-4572-825c-629714f62a60
 docker pull we2dinc/react-docker:main
-cd /home/ubuntu/build
-docker-compose up
+docker run -d -it -p 80:80 --name react-docker we2dinc/react-docker:main
 
